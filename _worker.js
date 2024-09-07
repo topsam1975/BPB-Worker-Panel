a9cf1d6db0054245b5ec6b97c08a295c6485032a
topsam1975 <83820274+topsam1975@users.noreply.github.com>
2024-09-08 07:18:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '345f0b97-a3ea-4c0a-909c-1938e927a001';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
